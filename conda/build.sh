8393f73d71d24a13cf98ea2bc5433b0b5c2e9bf7
Patrick Snape <patricksnape@gmail.com>
2014-11-18 12:54:48 +0000

    OK, not sure how to get around this...
    Build both static and shared because some libraries need one
    and some the other.

--- conda/build.sh
+++ conda/build.sh
@@ -13,12 +13,31 @@ patch -p0 < metis5_idx.patch
 
 cd build
 
+# Create the static libraries
 cmake .. \
 -DLIB_POSTFIX="" \
 -DBUILD_METIS=1 \
 -DGKLIB_PATH="$RECIPE_DIR/../metis/GKlib" \
 -DCMAKE_INSTALL_PREFIX=$PREFIX
-#-DSHARED=1
+
+# I have no idea how to stop the CMake from putting -lrt
+# inside the link.txt, so I've had to improvise by sed'ing it out
+if [ "$(uname -s)" == "Darwin" ]; then
+  sed -i"bak" 's/-lrt//' SuiteSparse/UMFPACK/CMakeFiles/umfpack.dir/link.txt
+  sed -i"bak" 's/-lrt//' SuiteSparse/SPQR/CMakeFiles/spqr.dir/link.txt
+  sed -i"bak" 's/-lrt//' SuiteSparse/CHOLMOD/CMakeFiles/cholmod.dir/link.txt
+fi
+
+make -j$CPU_COUNT
+make install
+
+# Create the SHARED libraries
+cmake .. \
+-DSHARED=1 \
+-DLIB_POSTFIX="" \
+-DBUILD_METIS=1 \
+-DGKLIB_PATH="$RECIPE_DIR/../metis/GKlib" \
+-DCMAKE_INSTALL_PREFIX=$PREFIX
 
 # I have no idea how to stop the CMake from putting -lrt
 # inside the link.txt, so I've had to improvise by sed'ing it out
@@ -33,3 +52,4 @@ make install
 
 # Metis seems to want to make this lib64 folder?
 rm -fr $PREFIX/lib64
+
