7bccb8b3aa90ab77eb9d0864a04ecad8f0cacac4
Patrick Snape <patricksnape@gmail.com>
2014-11-20 14:41:13 +0000

    Trying to remove Metis from autobuilding

--- conda/build.sh
+++ conda/build.sh
@@ -4,10 +4,6 @@ cp -r "$RECIPE_DIR/../" .
 
 mkdir build
 
-if [ "$(uname -s)" == "Darwin" ]; then
-  mv "./SuiteSparse/SuiteSparse_config/SuiteSparse_config_Mac.mk" "./SuiteSparse/SuiteSparse_config/SuiteSparse_config.mk"
-fi
-
 # Patch for Metis 5
 patch -p0 < metis5_idx.patch
 
@@ -16,27 +12,29 @@ cd build
 # Create the static libraries
 cmake .. \
 -DLIB_POSTFIX="" \
--DBUILD_METIS=1 \
--DGKLIB_PATH="$RECIPE_DIR/../metis/GKlib" \
--DCMAKE_INSTALL_PREFIX=$PREFIX
+-DCMAKE_INSTALL_PREFIX=$PREFIX \
+-DUSE_METIS=0 \
+-DMETIS_LIB_PATH="$LIBRARY_PATH/libmetis.a" \
+-DMETIS_INCLUDE_PATH=$INCLUDE_PATH
 
 # I have no idea how to stop the CMake from putting -lrt
 # inside the link.txt, so I've had to improvise by sed'ing it out
-if [ "$(uname -s)" == "Darwin" ]; then
-  sed -i"bak" 's/-lrt//' SuiteSparse/UMFPACK/CMakeFiles/umfpack.dir/link.txt
-  sed -i"bak" 's/-lrt//' SuiteSparse/SPQR/CMakeFiles/spqr.dir/link.txt
-  sed -i"bak" 's/-lrt//' SuiteSparse/CHOLMOD/CMakeFiles/cholmod.dir/link.txt
-fi
+#if [ "$(uname -s)" == "Darwin" ]; then
+#  sed -i"bak" 's/-lrt//' SuiteSparse/UMFPACK/CMakeFiles/umfpack.dir/link.txt
+#  sed -i"bak" 's/-lrt//' SuiteSparse/SPQR/CMakeFiles/spqr.dir/link.txt
+#  sed -i"bak" 's/-lrt//' SuiteSparse/CHOLMOD/CMakeFiles/cholmod.dir/link.txt
+#fi
 
 make -j$CPU_COUNT
 make install
 
 # Create the SHARED libraries
 cmake .. \
--DSHARED=1 \
+-DBUILD_SHARED_LIBS=1 \
 -DLIB_POSTFIX="" \
--DBUILD_METIS=1 \
--DGKLIB_PATH="$RECIPE_DIR/../metis/GKlib" \
+-DUSE_METIS=0 \
+-DMETIS_LIB_PATH="$LIBRARY_PATH/libmetis.so" \
+-DMETIS_INCLUDE_PATH=$INCLUDE_PATH \
 -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 # I have no idea how to stop the CMake from putting -lrt
@@ -49,7 +47,4 @@ fi
 
 make -j$CPU_COUNT
 make install
-
-# Metis seems to want to make this lib64 folder?
-rm -fr $PREFIX/lib64
 
