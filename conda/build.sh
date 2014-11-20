5eea5139acca99504f752503f03e60eae36df179
Patrick Snape <patricksnape@gmail.com>
2014-11-20 19:10:55 +0000

    OK, trying to use metis and create shared libraries
    Metis is seperate, but the problem is that I can't get shared
    libraries to build properly. Ceres seems to complain that they
    are linked properly. Not sure who's fault this is. For now,
    I'm hacking it by creating relocatable static libraries

--- conda/build.sh
+++ conda/build.sh
@@ -13,29 +13,9 @@ cd build
 cmake .. \
 -DLIB_POSTFIX="" \
 -DCMAKE_INSTALL_PREFIX=$PREFIX \
--DUSE_METIS=0 \
--DMETIS_LIB_PATH="$LIBRARY_PATH/libmetis.a" \
+-DUSE_METIS=1 \
+-DMETIS_LIB="$LIBRARY_PATH/libmetis.a" \
 -DMETIS_INCLUDE_PATH=$INCLUDE_PATH
-
-# I have no idea how to stop the CMake from putting -lrt
-# inside the link.txt, so I've had to improvise by sed'ing it out
-#if [ "$(uname -s)" == "Darwin" ]; then
-#  sed -i"bak" 's/-lrt//' SuiteSparse/UMFPACK/CMakeFiles/umfpack.dir/link.txt
-#  sed -i"bak" 's/-lrt//' SuiteSparse/SPQR/CMakeFiles/spqr.dir/link.txt
-#  sed -i"bak" 's/-lrt//' SuiteSparse/CHOLMOD/CMakeFiles/cholmod.dir/link.txt
-#fi
-
-make -j$CPU_COUNT
-make install
-
-# Create the SHARED libraries
-cmake .. \
--DBUILD_SHARED_LIBS=1 \
--DLIB_POSTFIX="" \
--DUSE_METIS=0 \
--DMETIS_LIB_PATH="$LIBRARY_PATH/libmetis.so" \
--DMETIS_INCLUDE_PATH=$INCLUDE_PATH \
--DCMAKE_INSTALL_PREFIX=$PREFIX
 
 # I have no idea how to stop the CMake from putting -lrt
 # inside the link.txt, so I've had to improvise by sed'ing it out
